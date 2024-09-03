3d031a638ee6a919fce9e2301fb7af160b293289
YuriFont <yurifontenelesilva180303@gmail.com>
2024-09-03 10:01:26 -0300

    :construction: config intelij

---
+++ Desafio_Target/src/main/java/ex02/SequenciaFibonacci.java
@@ -0,0 +1,34 @@
+package ex02;
+import  java.util.Scanner;
+
+public class SequenciaFibonacci {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new Scanner(System.in);
+        int     number, next, a = 0, b = 1;
+        boolean foundNum = false;
+
+        System.out.print("Digite um número para verificar na sequência de Fibonacci: ");
+        number = sc.nextInt();
+
+        if (number == 0) {
+            System.out.println("O número 0 pertence à sequencia de Fibonacci.");
+            sc.close();
+            return ;
+        }
+
+        while (b <= number && !foundNum) {
+            if (b == number)
+                foundNum = true;
+            next = a + b;
+            a = b;
+            b = next;
+        }
+
+        if (foundNum)
+            System.out.println("O número " + number + " pertence à sequência de Fibonacci.");
+        else
+            System.out.println("O número " + number + " não pertence à sequência de Fibonacci.");
+
+        sc.close();
+    }
+}
---
+++ Desafio_Target/src/main/java/ex03/FaturamentoDistribuidora.java
@@ -0,0 +1,55 @@
+package ex03;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FaturamentoDistribuidora {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File("C:\\Users\\yurif\\OneDrive\\Área de Trabalho\\Projetos\\Desafio_Target\\src\\main\\java\\ex03\\faturamento.json"));
+
+            double menorFaturamento = Double.MAX_VALUE;
+            double maiorFaturamento = Double.MIN_VALUE;
+            double somaFaturamento = 0;
+            int diasComFaturamento = 0;
+
+            Iterator<JsonNode> elements = rootNode.elements();
+            while (elements.hasNext()) {
+                JsonNode node = elements.next();
+                double faturamentoDiario = node.get("faturamento").asDouble();
+
+                if (faturamentoDiario > 0) {
+                    if (faturamentoDiario < menorFaturamento)
+                        menorFaturamento = faturamentoDiario;
+                    else if (faturamentoDiario > maiorFaturamento)
+                        maiorFaturamento = faturamentoDiario;
+                    somaFaturamento += faturamentoDiario;
+                    diasComFaturamento++;
+                }
+            }
+
+            double mediaMensal = somaFaturamento / diasComFaturamento;
+            int diasAcimaDaMedia = 0;
+
+            elements = rootNode.elements();
+            while (elements.hasNext()) {
+                JsonNode node = elements.next();
+                double faturamentoDiario = node.get("faturamento").asDouble();
+                if (faturamentoDiario > mediaMensal)
+                    diasAcimaDaMedia++;
+            }
+
+            System.out.println("Menor faturamento: " + menorFaturamento);
+            System.out.println("Maior faturamento: " + maiorFaturamento);
+            System.out.print("Dias com faturamento acima da média: " + diasAcimaDaMedia);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
